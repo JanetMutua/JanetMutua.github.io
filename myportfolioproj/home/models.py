f728cb1327bcefa0551a2ff81fa1af8a92417e10
JanetMutua <janetmutua04@gmail.com>
2021-07-27 16:40:23 +0300

    djangoweb

--- myportfolioproj/home/models.py
+++ myportfolioproj/home/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class skills:
+    image = models.ImageField(upload_to = "images/")
+    title = models.CharField(max_length=50)
+    summary = models.CharField(max_length=200)
--- myportfolioproj/home/views.py
+++ myportfolioproj/home/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def janet(request):
+    return render(request, 'home/janet.html')
