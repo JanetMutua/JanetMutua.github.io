b61aaeb733d61155b8dcb4c33070b4c6ab0f6ef3
Janet Mutua <janetmutua04@gmail.com>
2022-02-17 09:37:32 +0300

    Delete myportfolioproj directory

--- myportfolioproj/home/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class skills:
-    image = models.ImageField(upload_to = "images/")
-    title = models.CharField(max_length=50)
-    summary = models.CharField(max_length=200)
--- myportfolioproj/home/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-
-def janet(request):
-    return render(request, 'home/janet.html')
